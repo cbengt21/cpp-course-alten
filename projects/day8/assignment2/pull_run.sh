c6fb0c520b2e5825bc262c53eab2bd346e481ffb
Christopher Bengtsson <christopher.bengtsson@volvocars.com>
2021-10-13 15:58:50 +0200

    comented unused bash

--- projects/day8/assignment2/pull_run.sh
+++ projects/day8/assignment2/pull_run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-git clone \
-    --depth 1 \
-    --filter=blob:none \
-    --no-checkout \
-    git@github.com:cbengt21/cpp-course-alten.git;
- cd cpp-course-alten
- git checkout main -- projects/miniProject/
+#git clone \
+#    --depth 1 \
+#    --filter=blob:none \
+#    --no-checkout \
+#    git@github.com:cbengt21/cpp-course-alten.git;
+# cd cpp-course-alten
+# git checkout main -- projects/miniProject/
 
-cd miniProject/miniProject/
-g++ main.cpp
-a.out
+#cd miniProject/miniProject/
+#g++ main.cpp
+#a.out
